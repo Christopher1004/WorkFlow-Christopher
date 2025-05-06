5ee04ea8f6015e99a3b01f608d9eb8ef097f12dc
Christopher João da Cruz Ferreira <141236105+Christopher1004@users.noreply.github.com>
2025-05-06 19:12:42 -0300

    Update cadastro.js

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -8,6 +8,22 @@ const supabase = createClient(supabaseURL, supabaseChave)
 
 async function cadastroDados(emailFree, senhaFree, dataNascimentoFree) {
     try {
+
+        const { data: emailExistente, error: erroConsulta } = await supabase
+            .from('freelancer')
+            .select()
+            .eq('email', emailFree)
+            .single()
+            console.log('verificado')
+        if (erroConsulta && erroConsulta.code !== 'PGRST116') {
+            console.error('Erro ao consultar email:', erroConsulta);
+            return false;
+        }
+        if (emailFree) {
+            alert('Este e-mail já está cadastrado.')
+            return false;
+        }
+
         const { data, error } = await supabase
             .from('freelancer')
             .insert([{
@@ -21,16 +37,13 @@ async function cadastroDados(emailFree, senhaFree, dataNascimentoFree) {
                 foto_perfil: null,
                 datanascimento: dataNascimentoFree
             }])
-        if (error) {
-            console.error('Erro ao enviar dados: ', error)
-            return false
-        }
         console.log('Dados enviados com sucesso', data)
         return true
+
     }
     catch (err) {
         console.error('Erro inesperado', err)
-        return null
+        return false
     }
 }
 const form = document.getElementById('formfreelancer')
@@ -47,18 +60,22 @@ form.addEventListener('submit', async (event) => {
     const confirmarSenha = inputConfirmarSenha.value;
     const dataNascimento = inputDataNascimento.value;
 
+    
+    if(senha == ''){
+        alert('por favor preencha a senha')
+        return 
+    }
+    else if (confirmarSenha == ''){
+        alert('preencha a confirmação da  senha')
+    } 
     if (senha != confirmarSenha) {
         alert('as senhas nao coincidem')
         return
     }
-
     const resultado = await cadastroDados(email, senha, dataNascimento)
     if (resultado) {
         alert('dados enviados com sucesso')
         form.reset();
-    }
-    else {
-        alert('erro ao enviar dados')
     }
 
 })
