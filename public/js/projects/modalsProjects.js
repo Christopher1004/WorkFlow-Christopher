3f1c11af5f825567d7ad99051048f28aecbd0831
CarlosMattei <carlosmattei2007@gmail.com>
2025-05-23 11:15:46 -0300

    alterações no modal de projeto

--- public/js/projects/modalsProjects.js
+++ public/js/projects/modalsProjects.js
@@ -1,13 +1,16 @@
-const cards = document.querySelectorAll('.card_projeto')
+const cards = document.querySelectorAll('.card_projeto');
 
 cards.forEach(card => {
-    card.addEventListener('click', () =>{
-        document.getElementById('modal').style.display = 'flex'
-    })
-})
+    card.addEventListener('click', () => {
+        document.getElementById('modal').style.display = 'flex';
+        document.body.style.overflow = 'hidden'; // Correção aqui
+    });
+});
 
-const modal = document.getElementById('modal')
+const modal = document.getElementById('modal');
 modal.addEventListener('click', (event) => {
-    if(event.target === modal)
-        modal.style.display = 'none'
-})
+    if (event.target === modal) {
+        modal.style.display = 'none';
+        document.body.style.overflow = ''; // Restaura o scroll ao fechar
+    }
+});
