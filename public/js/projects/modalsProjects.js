1417f9b3a46316f32896e3b3881817fbc3b03700
Christopher João da Cruz Ferreira <141236105+Christopher1004@users.noreply.github.com>
2025-05-29 19:03:53 -0300

    Update Active
    adição de código para trocar o update dos botões de categorias do index

--- public/js/projects/modalsProjects.js
+++ public/js/projects/modalsProjects.js
@@ -14,3 +14,17 @@ modal.addEventListener('click', (event) => {
         document.body.style.overflow = ''; // Restaura o scroll ao fechar
     }
 });
+
+const botoesCategorias = document.querySelector('.categorias')
+
+botoesCategorias.addEventListener('click', (event) => {
+    if(event.target.tagName === 'BUTTON'){
+        botoesCategorias.querySelectorAll('button').forEach(btn => {
+            btn.classList.remove('button_categoria-active')
+            btn.classList.add('button_categoria')
+        })
+
+        event.target.classList.add('button_categoria-active')
+        event.target.classList.remove('button_categoria')
+    }
+})
