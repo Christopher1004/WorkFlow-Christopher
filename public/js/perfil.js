8480fa87408bd1a5a9eb3fb9df6f379aed4e1972
yagozeppini <yago.groot@gmail.com>
2025-06-25 14:20:13 -0300

    botao excluir e editar beta

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -26,41 +26,65 @@ const abas = {
 
 let tipoUsuario = null;
 
-function criarCardProjeto(id, { titulo, descricao, capaUrl, dataCriacao, userId }, aba = 'projetos') {
-    const card = document.createElement('div');
-    card.className = 'card_projeto';
-    card.dataset.projetoId = id;
-    card.style.display = 'none';
+function criarCardProjeto(id, projeto, aba = 'projetos') {
+  const card = document.createElement('div');
+  card.className = 'card_projeto';
+  card.dataset.projetoId = id;
+  card.style.display = 'none';
 
-    const autorTexto = userId === perfilUserId ? 'você' : 'outra pessoa';
+  const autorTexto = projeto.userId === perfilUserId ? 'você' : 'outra pessoa';
 
-    card.innerHTML = `
-        <div class="capa">
-            <figure>
-                <img src="${capaUrl}" alt="" class="thumbnail">
-            </figure>
-            <div class="thumbnail-overlay">
-                <div class="project-overlay-content">
-                    <div class="like">
-                        <svg width="25" height="25" viewBox="-2 -2 28 28" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd"
-                                d="M10.2366 18.4731L18.35 10.3598L18.483 10.2267L18.4809 10.2246C20.6263 7.93881 20.5826 4.34605 18.35 2.11339C16.1173 -0.11928 12.5245 -0.16292 10.2387 1.98247L10.2366 1.98036L10.2366 1.98039L10.2366 1.98037L10.2345 1.98247C7.94862 -0.162927 4.35586 -0.119289 2.12319 2.11338C-0.109476 4.34605 -0.153114 7.93881 1.99228 10.2246L1.99017 10.2268L10.2365 18.4731L10.2366 18.4731Z"
-                                fill="none" stroke="black" />
-                        </svg>
-                    </div>
-                    <div class="project-title">
-                        <h1>${titulo}</h1>
-                    </div>
-                </div>
+  // Mostrar ícone de editar só se for dono do projeto E aba projetos
+  const mostrarEdit = projeto.userId === perfilUserId && aba === 'projetos';
+
+  card.innerHTML = `
+    <div class="capa">
+      <figure>
+        <img src="${projeto.capaUrl}" alt="" class="thumbnail">
+      </figure>
+      <div class="thumbnail-overlay">
+        <div class="project-overlay-content">
+          <div class="icons-column">
+            <div class="like" title="Curtir" style="cursor:pointer;">
+              <svg width="25" height="25" viewBox="-2 -2 28 28" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M10.2366 18.4731L18.35 10.3598L18.483 10.2267L18.4809 10.2246C20.6263 7.93881 20.5826 4.34605 18.35 2.11339C16.1173 -0.11928 12.5245 -0.16292 10.2387 1.98247L10.2366 1.98036L10.2366 1.98039L10.2366 1.98037L10.2345 1.98247C7.94862 -0.162927 4.35586 -0.119289 2.12319 2.11338C-0.109476 4.34605 -0.153114 7.93881 1.99228 10.2246L1.99017 10.2268L10.2365 18.4731L10.2366 18.4731Z"
+                  fill="none" stroke="black" />
+              </svg>
             </div>
+            ${mostrarEdit ? `
+              <div class="edit" title="Editar" style="cursor:pointer;">
+                <svg width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" >
+                  <path d="M12 20h9" />
+                  <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z"/>
+                </svg>
+              </div>
+              <div class="delete" title="Excluir" style="cursor:pointer;">
+    <svg width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <polyline points="3 6 5 6 21 6"></polyline>
+      <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"></path>
+      <path d="M10 11v6"></path>
+      <path d="M14 11v6"></path>
+      <path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"></path>
+    </svg>
+  </div>
+            ` : ''}
+          </div>
+          <div class="project-title">
+            <h1>${projeto.titulo}</h1>
+          </div>
         </div>
-        <div class="autor">
-            <h2 class="username">Criado por ${autorTexto}</h2>
-        </div>
-    `;
-    containerCard.appendChild(card);
-    abas[aba].push(card);
+      </div>
+    </div>
+    <div class="autor">
+      <h2 class="username">Criado por ${autorTexto}</h2>
+    </div>
+  `;
+
+  containerCard.appendChild(card);
+  abas[aba].push(card);
 }
+
 
 function criarCardProposta(p, aba = 'projetos') {
     const tagsHtml = Array.isArray(p.tags) ? p.tags.map(tag => `<span class="tag">${tag}</span>`).join('') : '';
