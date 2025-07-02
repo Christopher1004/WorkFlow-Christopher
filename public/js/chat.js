64923c210b2ea92a4c655d1cdd4d0d1b153b6f1d
Christopher João da Cruz Ferreira <christopherjoao2007@gmail.com>
2025-07-02 17:49:02 -0300

    Mais Coisas do Chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,7 +5,7 @@ const db = getDatabase()
 const auth = getAuth()
 
 onAuthStateChanged(auth, (user) => {
-    if(user){
+    if (user) {
         const userId = user.uid
         const conversasRef = ref(db, `Conversas/${userId}`)
 
@@ -26,6 +26,37 @@ onAuthStateChanged(auth, (user) => {
                         <small>Clique para abrir</small>
                     </div>
                 `;
+
+                chatUser.addEventListener("click", () => {
+                    const header = document.querySelector(".chat-header");
+                    const messages = document.querySelector(".messages");
+                    const input = document.querySelector(".input-area");
+
+                    const headerImg = header.querySelector("img");
+                    const headerNome = header.querySelector("span");
+
+                    header.classList.remove("show");
+                    messages.classList.remove("show");
+                    input.classList.remove("show");
+
+                    setTimeout(() => {
+                        headerImg.src = dados.avatar;
+                        headerNome.textContent = dados.nome;
+
+                        document.querySelector(".nenhum-contato-selecionado").style.display = "none";
+
+                        header.style.display = "flex";
+                        input.style.display = "flex";
+                        messages.style.display = "flex";
+
+                        void header.offsetWidth;
+
+                        header.classList.add("show");
+                        messages.classList.add("show");
+                        input.classList.add("show");
+                    }, 100); 
+                });
+
                 sidebar.appendChild(chatUser)
             })
         })
